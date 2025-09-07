0c52308ed7cdc501df2f10cce6f4cd4e80362513
Ahmed Ahmed <164640064+Ahmed-Jafar-Ahmed@users.noreply.github.com>
2025-09-07 17:22:35 -0400

    Fixed some bugs and errors

--- C_Edit.py
+++ C_Edit.py
@@ -24,36 +24,39 @@ def cartoonify(ImagePath):
     if originalImage is None:
         print("Can not find any image. Choose appropriate file")
         sys.exit()
-    Resized1 = cv2.resize(originalImage, (960, 540))
+    Resized1 = cv2.resize(originalImage, (660, 940))
     #plt.imshow(Resized1)
     #plt.show()
     grayImage = cv2.cvtColor(originalImage, cv2.COLOR_RGB2GRAY)
-    Resized2 = cv2.resize(grayImage, (960, 540))
+    Resized2 = cv2.resize(grayImage, (660, 940))
     #plt.imshow(Resized2, cmap='gray')
     #plt.show()
     smoothed_image = cv2.medianBlur(grayImage, 5)
-    Resized3 = cv2.resize(smoothed_image, (960, 540))
+    Resized3 = cv2.resize(smoothed_image, (660, 940))
     #plt.imshow(Resized3, cmap='gray')
     #plt.show()
     edgeImage = cv2.adaptiveThreshold(smoothed_image, 255,
                                       cv2.ADAPTIVE_THRESH_MEAN_C,
                                       cv2.THRESH_BINARY, 9, 9)
-    Resized4 = cv2.resize(edgeImage, (960, 540))
+    Resized4 = cv2.resize(edgeImage, (660, 940))
     #plt.imshow(Resized4, cmap='gray')
     #plt.show()
     colorImage = cv2.bilateralFilter(originalImage, 9, 300, 300)
-    Resized5 = cv2.resize(colorImage, (960, 540))
+    Resized5 = cv2.resize(colorImage, (660, 940))
     #plt.imshow(Resized5)
     #plt.show()
     cartoonImage = cv2.bitwise_and(colorImage, colorImage, mask=edgeImage)
-    Resized6 = cv2.resize(cartoonImage, (960, 540))
+    Resized6 = cv2.resize(cartoonImage, (660, 940))
     #plt.imshow(Resized6)
     #plt.show()
     # Plotting the whole transition
     images = [Resized1, Resized2, Resized3, Resized4, Resized5, Resized6]
     fig, axes = plt.subplots(3, 2, figsize=(8, 8), subplot_kw={'xticks': [], 'yticks': []})
     for i, ax in enumerate(axes.flat):
-        ax.imshow(images[i], cmap='gray')
+        if len(images[i].shape) == 2:  # grayscale
+            ax.imshow(images[i], cmap='gray')
+        else:  # color
+            ax.imshow(images[i])
     plt.tight_layout()
     plt.show()
     return cartoonImage
